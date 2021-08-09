90db6e4da23038557abf99775b2881a414ce9691
Seleznev Sergey <seleznevsa@aldera.loc>
2021-08-09 13:09:01 +0700

    Add photo studio fake data

--- src/store/_fakeData/fakeData.js
+++ src/store/_fakeData/fakeData.js
@@ -138,3 +138,96 @@ export const profileData = {
     "Lfsdgag gsdgs dgf gds hsgfhhhhrhqegag gsdgwegbg fg sdfhs gh sj tjs jgf jtj t herahrehrehferdfh herhare hareh aerh rehare hare hre aharehaerh rehareh",
   userLanguages: "Русский, English",
 };
+
+export const photoStudioData = {
+  photoStudioList: [
+    {
+      studioId: 0,
+      photoTitle: photoTitle,
+      studioName: "Томсон",
+      numberOfHall: 2,
+      studioLocation: "г. Томск, ул. Говорова, 19в ст1 — 3 этаж",
+      studioArea: 125,
+      stidioRating: 4,
+      studioCost: 1300,
+    },
+    {
+      studioId: 1,
+      photoTitle: photoTitle,
+      studioName: "Томсон",
+      numberOfHall: 2,
+      studioLocation: "г. Томск, ул. Говорова, 19в ст1 — 3 этаж",
+      studioArea: 125,
+      stidioRating: 4,
+      studioCost: 1300,
+    },
+    {
+      studioId: 2,
+      photoTitle: photoTitle,
+      studioName: "Томсон",
+      numberOfHall: 2,
+      studioLocation: "г. Томск, ул. Говорова, 19в ст1 — 3 этаж",
+      studioArea: 125,
+      stidioRating: 4,
+      studioCost: 1300,
+    },
+    {
+      studioId: 3,
+      photoTitle: photoTitle,
+      studioName: "Томсон",
+      numberOfHall: 2,
+      studioLocation: "г. Томск, ул. Говорова, 19в ст1 — 3 этаж",
+      studioArea: 125,
+      stidioRating: 4,
+      studioCost: 1300,
+    },
+  ],
+  photoStudio: {
+    hallsPhotos: [photoTitle, photoTitle],
+    studioName: "Томсон",
+    studioLocation: "г. Томск, ул. Говорова, 19в ст1 — 3 этаж",
+    studioArea: 125,
+    stidioRating: 4,
+    studioCost: 1300,
+    studioWorkHours: "08:00 - 20:00 Понедельник - Воскресенье",
+    studioContactPhone: "+7-(906)-960-10-87",
+    studionSocialMedia: {
+      instagram: "https://www.instagram.com/",
+      facebook: "https://www.facebook.com/",
+      vkontakte: "https://vk.com/",
+    },
+  },
+  studioPhotoPackage: [photoTitle, photoTitle, photoTitle, photoTitle],
+  studioFeedbackList: [
+    {
+      feedbackId: 0,
+      authorId: 0,
+      authorName: "Альберт",
+      authorPhoto: photoTitle,
+      authorRating: 4,
+      createdAt: "21.04.2021",
+      feedbackText:
+        "Буду краток в форме изложения своего мнения о данном мероприятии: Мероприятие стоищее, не пожалел, что сходил!",
+    },
+    {
+      feedbackId: 1,
+      authorId: 1,
+      authorName: "Альберт",
+      authorPhoto: photoTitle,
+      authorRating: 4,
+      createdAt: "21.04.2021",
+      feedbackText:
+        "Буду краток в форме изложения своего мнения о данном мероприятии: Мероприятие стоищее, не пожалел, что сходил!",
+    },
+    {
+      feedbackId: 2,
+      authorId: 2,
+      authorName: "Альберт",
+      authorPhoto: photoTitle,
+      authorRating: 4,
+      createdAt: "21.04.2021",
+      feedbackText:
+        "Буду краток в форме изложения своего мнения о данном мероприятии: Мероприятие стоищее, не пожалел, что сходил!",
+    },
+  ],
+};
